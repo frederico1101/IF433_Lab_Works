1c8307b9eb9af033452cb8e47b0934ece646d693
Frederico Sarren <frederico@student.umn.ac.id>
2026-02-04 16:10:54 +0700

    week01: (tugas 2) implement discount logic

--- src/week01/GameStore.kt
+++ src/week01/GameStore.kt
@@ -3,4 +3,9 @@ package week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 650000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500_000) price * 80 / 100 else price * 90 / 100
