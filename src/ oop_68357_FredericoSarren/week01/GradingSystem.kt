a76c7a876cdee4bac7076e2847f23d9121244b53
Frederico Sarren <frederico@student.umn.ac.id>
2026-02-04 15:41:14 +0700

    week01: initial setup and procedural code

---
+++ src/ oop_68357_FredericoSarren/week01/GradingSystem.kt
@@ -0,0 +1,11 @@
+package oop_68357_FredericoSarren.week01
+
+fun main() {
+    var name: String = "frederico"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
+
+
