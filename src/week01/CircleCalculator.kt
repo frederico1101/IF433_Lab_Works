a9d635c8eb49e700055b8f1856556dd3503a6fa5
Frederico Sarren <frederico@student.umn.ac.id>
2026-02-04 16:04:14 +0700

    week01: (tugas 1) convert checkSize to expression body function

--- src/week01/CircleCalculator.kt
+++ src/week01/CircleCalculator.kt
@@ -1,20 +1,12 @@
 package oop_68357_FredericoSarren.week01
 
 fun main(args: Array<String>) {
-    // Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 3.14
-    // Calculation
-    var area: Double = pi * radius * radius
-// Output Concatenation
-    println("Radius: " + radius + ", Area: " + area)
-    // Logic check
-    checkSize(area)
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
+    println("Radius: $radius, Area: $area")
+    println(checkSize(area))
 }
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    } else {
-        println("This is a Small Circle")
-    }
-}
+
+fun checkSize(area: Double) =
+    if (area > 100) "This is a Big Circle" else "This is a Small Circle"
