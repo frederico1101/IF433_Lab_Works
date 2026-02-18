5ef0287b16dcf6eced0971b6cd63c38753ee6f66
Frederico Sarren <frederico@student.umn.ac.id>
2026-02-18 15:50:48 +0700

    week03: (task 1) create Weapon Class with validation

---
+++ src/week03/weapon.kt
@@ -0,0 +1,27 @@
+package week03
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            when {
+                value < 0 -> {
+                    println("WARNING: Damage tidak boleh negatif! Nilai tidak diubah.")
+                }
+                value > 1000 -> {
+                    println("WARNING: Damage terlalu besar! Di-set ke 1000.")
+                    field = 1000
+                }
+                else -> {
+                    field = value
+                }
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
