981edd4edfa90f8c8a31d7bb3e5e5e75f93d4e9d
Frederico Sarren <frederico@student.umn.ac.id>
2026-03-11 15:12:47 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_68357_FredericoSarren/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_68357_FredericoSarren.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa Kamera terbuka dan sensor aktif.") }
+    }
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.")}
+}
