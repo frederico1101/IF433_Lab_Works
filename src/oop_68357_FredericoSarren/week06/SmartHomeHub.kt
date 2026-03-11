6070af168ff7d1b0b5a8f92f780a8d83d06483cc
Frederico Sarren <frederico@student.umn.ac.id>
2026-03-11 15:30:53 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_68357_FredericoSarren/week06/SmartHomeHub.kt
@@ -0,0 +1,31 @@
+package oop_68357_FredericoSarren.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
