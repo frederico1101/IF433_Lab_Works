b94ccf13a9704de94cc802839e257d6b83a55566
Frederico Sarren <frederico@student.umn.ac.id>
2026-03-11 15:28:35 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_68357_FredericoSarren/week06/SmartLamp.kt
@@ -0,0 +1,15 @@
+package oop_68357_FredericoSarren.week06
+
+class SmartLamp(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("Lampu $name dinyalakan.")
+    }
+
+    override fun turnOff() {
+        println("Lampu $name dimatikan.")
+    }
+}
