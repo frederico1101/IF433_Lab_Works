7486dd2d5c3ccb2d404ef44c177ca00e133b7c45
Frederico Sarren <frederico@student.umn.ac.id>
2026-03-14 20:13:27 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_68357_FredericoSarren/week04/Employee.kt
@@ -0,0 +1,15 @@
+package oop_68357_FredericoSarren.week04
+
+open class Employee(
+    val name: String,
+    val baseSalary: Int
+) {
+
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (baseSalary * 10) / 100
+    }
+}
