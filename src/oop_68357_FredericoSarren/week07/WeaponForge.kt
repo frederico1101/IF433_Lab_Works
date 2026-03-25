77b001426b53621019fdd461b368a8a81037fcc9
Frederico Sarren <frederico@student.umn.ac.id>
2026-03-25 15:20:41 +0700

    week07: (task) implement factory pattern in WeaponForge

---
+++ src/oop_68357_FredericoSarren/week07/WeaponForge.kt
@@ -0,0 +1,24 @@
+package oop_68357_FredericoSarren.week07
+
+class Weapon private constructor(
+    val item: GameItem,
+    val durability: Int
+) {
+
+    companion object {
+
+        fun forgeStarterSword(): Weapon {
+            return Weapon(
+                GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON),
+                50
+            )
+        }
+
+        fun forgeEpicSword(): Weapon {
+            return Weapon(
+                GameItem("Pedang Naga Legendaris", 100, ItemRarity.EPIC),
+                200
+            )
+        }
+    }
+}
