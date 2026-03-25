2fbcc0f4ea755295f9f57b1d0f6ea610d78753ea
Frederico Sarren <frederico@student.umn.ac.id>
2026-03-25 15:21:17 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_68357_FredericoSarren/week07/BattleEvent.kt
@@ -0,0 +1,12 @@
+package oop_68357_FredericoSarren.week07
+
+sealed class BattleState {
+
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+
+    data class LootDropped(val item: GameItem) : BattleState()
+
+    data class GameOver(val reason: String) : BattleState()
+
+    object SafeZone : BattleState()
+}
