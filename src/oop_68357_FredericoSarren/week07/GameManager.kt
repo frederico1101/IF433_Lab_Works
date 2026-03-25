94cb0d41b5c9466e408a72f71da08c81f2a4764e
Frederico Sarren <frederico@student.umn.ac.id>
2026-03-25 15:18:08 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_68357_FredericoSarren/week07/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_68357_FredericoSarren.week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
