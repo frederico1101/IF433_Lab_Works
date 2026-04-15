8d31b16c995af68ee632d8432302ec7b328438bf
Frederico Sarren <frederico@student.umn.ac.id>
2026-04-15 15:40:41 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_68357_FredericoSarren/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_68357_FredericoSarren.week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
