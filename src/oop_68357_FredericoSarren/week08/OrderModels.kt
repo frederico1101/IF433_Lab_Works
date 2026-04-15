0cf729d4c703768eb6b67165328ea798f9422d9c
Frederico Sarren <frederico@student.umn.ac.id>
2026-04-15 15:07:40 +0700

    week08: create nested classes for Order

---
+++ src/oop_68357_FredericoSarren/week08/OrderModels.kt
@@ -0,0 +1,7 @@
+package oop_68357_FredericoSarren.week08
+
+class City (val name: String)
+class Address (val city: City?)
+class DeliveryDetails (val address: Address?)
+class Order (val deliveryDetails: DeliveryDetails?, val totalPrice : Int?)
+
