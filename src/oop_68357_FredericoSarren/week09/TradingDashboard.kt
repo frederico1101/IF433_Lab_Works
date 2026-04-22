acc992124fc1a24a848dc8e0d031f661d79c3cc8
Frederico Sarren <frederico@student.umn.ac.id>
2026-04-22 15:12:23 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_68357_FredericoSarren/week09/TradingDashboard.kt
+++ src/oop_68357_FredericoSarren/week09/TradingDashboard.kt
@@ -11,44 +11,8 @@ fun main() {
         TradeLog("BTCUSDT", "LONG", 10, -2.0, "OPEN")
     )
 
-
+    //pipeline 1
     val closedTrades = tradeHistory
         .filter { it.status == "CLOSED" }
 
-    val winningTrades = closedTrades
-        .filter { it.roe > 0 }
-
-    val losingTrades = closedTrades
-        .filter { it.roe <= 0 }
-
-    val topPerformersString = winningTrades
-        .sortedByDescending { it.roe }
-        .map {
-            "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
-        }
-
-    val worstPerformersString = losingTrades
-        .sortedBy { it.roe }
-        .map {
-            "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
-        }
-
-    val uniquePairs = tradeHistory
-        .map { it.pair }
-        .toSet()
-
-    println("=== CRYPTO TRADING DASHBOARD ===")
-
-    println("\n--- TOP PERFORMERS ---")
-    topPerformersString.forEach {
-        println(it)
-    }
-
-    println("\n--- WORST PERFORMERS ---")
-    worstPerformersString.forEach {
-        println(it)
-    }
-
-    println("\n--- TRADED PAIRS ---")
-    println(uniquePairs)
 }
