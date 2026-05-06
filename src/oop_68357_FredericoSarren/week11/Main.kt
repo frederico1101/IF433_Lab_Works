df0d61a0aacf416f936e5317fd86081c7f9c94a6
Frederico Sarren <frederico@student.umn.ac.id>
2026-05-06 15:14:50 +0700

    week11: test extension functions in main

---
+++ src/oop_68357_FredericoSarren/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_68357_FredericoSarren.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
