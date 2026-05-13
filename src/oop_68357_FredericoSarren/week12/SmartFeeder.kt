8566c87b5d9f38e038dbc36739e5d28243206e2d
Frederico Sarren <frederico@student.umn.ac.id>
2026-05-13 12:35:38 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_68357_FredericoSarren/week12/SmartFeeder.kt
@@ -0,0 +1,15 @@
+package oop_68357_FredericoSarren.week12
+
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+
+    return availableGram
+}
