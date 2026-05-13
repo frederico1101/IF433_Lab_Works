eb9dd320dcc4fdd88086651d8d45fdda68cebd5a
Frederico Sarren <frederico@student.umn.ac.id>
2026-05-13 12:43:15 +0700

    week12: (task) handle feeding failure using onFailure

--- src/oop_68357_FredericoSarren/week12/SmartFeeder.kt
+++ src/oop_68357_FredericoSarren/week12/SmartFeeder.kt
@@ -64,5 +64,14 @@ fun main() {
             "Makan sore sukses! " +
                     "Sisa stok kibble: $currentKibbleStock gr"
         )
+    }.onFailure { error ->
+
+        println(
+            "Peringatan ke Pemilik: ${error.message}"
+        )
+
+        println(
+            "(Opsional: Berikan chicken jerky secara manual)"
+        )
     }
 }
