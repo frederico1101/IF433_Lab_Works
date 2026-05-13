1f4e013085198836721b144b029b5bc5ed5d2adf
Frederico Sarren <frederico@student.umn.ac.id>
2026-05-13 12:41:28 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_68357_FredericoSarren/week12/SmartFeeder.kt
+++ src/oop_68357_FredericoSarren/week12/SmartFeeder.kt
@@ -49,4 +49,14 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+
+    println("\n=== JADWAL MAKAN SORE ===")
+
+    runCatching {
+        dispenseKibble(
+            requestedGram = 30,
+            availableGram = 1000,
+            isJammed = false
+        )
+    }
 }
