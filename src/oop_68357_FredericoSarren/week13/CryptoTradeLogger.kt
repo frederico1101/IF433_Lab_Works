ed8573668a5ae36856e187fd7dcf156fb4418165
Frederico Sarren <frederico@student.umn.ac.id>
2026-05-20 14:41:26 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_68357_FredericoSarren/week13/CryptoTradeLogger.kt
+++ src/oop_68357_FredericoSarren/week13/CryptoTradeLogger.kt
@@ -107,4 +107,11 @@ fun main() {
         .appendText(
             "CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n"
         )
+    val loadedData = loadTrades(
+        "crypto_trades.csv"
+    )
+
+    val totalPnl = loadedData.sumOf {
+        it.pnl
+    }
 }
