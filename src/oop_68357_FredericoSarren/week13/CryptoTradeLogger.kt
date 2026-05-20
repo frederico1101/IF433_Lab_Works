861f8aee77f05e763b4140be355bb71d1d26278a
Frederico Sarren <frederico@student.umn.ac.id>
2026-05-20 14:38:38 +0700

    week13: (task) implement fromCsvTrade with split

--- src/oop_68357_FredericoSarren/week13/CryptoTradeLogger.kt
+++ src/oop_68357_FredericoSarren/week13/CryptoTradeLogger.kt
@@ -12,3 +12,16 @@ fun TradeRecord.toCsv(): String {
 
     return "$id,$symbol,$type,$margin,$pnl"
 }
+
+fun fromCsvTrade(line: String): TradeRecord? {
+
+    val data = line.split(",")
+
+    return TradeRecord(
+        data[0].toInt(),
+        data[1],
+        data[2],
+        data[3].toDouble(),
+        data[4].toDouble()
+    )
+}
