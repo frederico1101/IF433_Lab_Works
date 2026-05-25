72b26d1944d7d8651608a7dbe7ea0f8ac4a0d17b
Frederico Sarren <frederico@student.umn.ac.id>
2026-05-25 09:55:30 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_68357_FredericoSarren/week14/EcommerceTask.kt
+++ src/oop_68357_FredericoSarren/week14/EcommerceTask.kt
@@ -28,3 +28,54 @@ class EmailNotifier : NotificationService {
         println("Email terkirim: $message")
     }
 }
+
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
+
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price
+}
+
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price * 0.90
+}
+
+class BetterOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+
+    fun processOrder(
+        itemName: String,
+        basePrice: Double,
+        customerType: String,
+        pricingStrategy: PricingStrategy
+    ) {
+
+        val finalPrice = pricingStrategy.calculate(basePrice)
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+
+        repo.saveOrder(itemName, finalPrice, customerType)
+
+        notifier.sendNotification(
+            "Pesanan $itemName Anda telah dikonfirmasi!"
+        )
+    }
+}
+
+fun main() {
+
+    val processor = BetterOrderProcessor(
+        repo = CsvOrderRepository(),
+        notifier = EmailNotifier()
+    )
+
+    processor.processOrder(
+        itemName = "Keyboard Gaming",
+        basePrice = 350000.0,
+        customerType = "VIP",
+        pricingStrategy = VipPricing()
+    )
+}
